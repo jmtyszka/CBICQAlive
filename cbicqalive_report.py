5589920e9b02810c3e8538c616e679d447c7a9f9
Mike Tyszka <mike.tyszka@gmail.com>
2016-07-14 16:37:45 -0700

    Add calculations of outlier threshold and number of outliers to summary stats. Clean up HTML report output. Ready for batch summary.

--- cbicqalive_report.py
+++ cbicqalive_report.py
@@ -1,4 +1,4 @@
-#!/opt/local/bin/python
+#!/usr/bin/env python3
 #
 # Create daily QA HTML report
 #
@@ -27,10 +27,10 @@
 #
 # Copyright 2013-2014 California Institute of Technology.
 
-import sys
 import os
 import string
 import argparse
+import numpy as np
 from pylab import *
 
 # Define template
@@ -40,60 +40,125 @@ TEMPLATE_FORMAT = """
 <head>
 <STYLE TYPE="text/css">
 BODY {
-  font-family    : sans-serif;
+  font-family    : arial, sans-serif;
 }
-td {
+td, th {
   padding-left   : 10px;
   padding-right  : 10px;
   padding-top    : 0px;
   padding-bottom : 0px;
-  vertical-align : top;
+  text-align     : "left";
 }
 </STYLE>
 </head>
 
 <body>
 
-<h1 style="background-color:#E0E0FF">CBIC Daily QA Report</h1>
+<h1 style="background-color:#E0E0FF">CBIC In Vivo Quality Assurance</h1>
 
+<h3> Dataset Information </h3>
 <table>
-<tr>
-
-<!-- Scanner and acquisition info -->
-<td>
-  <table>
-  <tr> <td> <b>Dummy</b> <td bgcolor="#E0FFE0"> <b>XXXXXX</b> </tr>
-  </table>
-</td>
-
-<br><br>
-
-<table>
-
-<tr>
-<td> <h3>Temporal Summary Images and Masks</h3>
-<td> <h3>Signal, Drift and Noise</h3>
-</tr>
-
-<tr>
-
-<!-- Temporal summary images and masks -->
-<td valign="top">
-<b>tSNR</b><br> <img src=qa_tsnr_ortho.png /><br><br>
-<b>tMean</b><br> <img src=qa_tmean_ortho.png /><br><br>
-<b>tSD</b><br> <img src=qa_tsd_ortho.png /><br><br>
-<b>Region Mask</b><br> <img src=qa_mask_ortho.png /><br><br>
-
-<!-- Motion timeseries -->
-<td valign="top">
-<img src=qa_mcf_trans.png /><br><br>
-<img src=qa_mcf_rot.png /><br><br>
-
-</tr>
+    <tr>
+        <td> QA Directory </td>
+        <td> $qa_dir_abs </td>
+    </tr>
+    <tr>
+        <td> Repetition Time (seconds) </td>
+        <td> $TR_secs </td>
+    </tr>
+    <tr>
+        <td> Image Volumes </td>
+        <td> $N_vols </td>
+    </tr>
 
 </table>
 
+<h3> Summary Statistics </h3>
+<table>
+    <tr>
+        <td> <b> Parameter </b> </td>
+        <td> <b> Mean </b> </td>
+        <td> <b> Threshold </b> </td>
+        <td> <b> Outliers </b> </td>
+        <td> </td>
+    </tr>
+    <tr>
+        <td> sMean Brain Signal </td>
+        <td> $brain_tmean </td>
+        <td> $brain_thresh </td>
+        <td> $brain_nout </td>
+        <td> $brain_pout% </td>
+    </tr>
+    <tr>
+        <td> sMean Nyquist Ghost Signal</td>
+        <td> $ghost_tmean </td>
+        <td> $ghost_thresh </td>
+        <td> $ghost_nout </td>
+        <td> $ghost_pout% </td>
+    </tr>
+    <tr>
+        <td> sMean Air Signal</td>
+        <td> $air_tmean </td>
+        <td> $air_thresh </td>
+        <td> $air_nout </td>
+        <td> $air_pout% </td>
+    </tr>
+    <tr>
+        <td> DVARS</td>
+        <td> $dvars_tmean </td>
+        <td> $dvars_thresh </td>
+        <td> $dvars_nout </td>
+        <td> $dvars_pout% </td>
+    </tr>
+    <tr>
+        <td> F-F Displacement (microns)</td>
+        <td> $dd_um_tmean </td>
+        <td> $dd_um_thresh </td>
+        <td> $dd_um_nout </td>
+        <td> $dd_um_pout% </td>
+    </tr>
+    <tr>
+        <td> F-F Rotation (mdeg)</td>
+        <td> $dr_mdeg_tmean </td>
+        <td> $dr_mdeg_thresh </td>
+        <td> $dr_mdeg_nout </td>
+        <td> $dr_mdeg_pout% </td>
+    </tr>
+</table>
+
+<h3> Temporal Summary Images </h3>
+<table>
+    <tr>
+        <td> <b>SNFR</b><br> <img src=qa_tsnfr_ortho.png /> </td>
+    </tr>
+    <tr>
+        <td> <br><b>Mean Signal</b><br> <img src=qa_tmean_ortho.png /> </td>
+    </tr>
+    </tr>
+        <td> <br><b>Fluctuation Noise SD</b><br> <img src=qa_tsd_ortho.png /> </td>
+    </tr>
+    <tr>
+        <td> <br><b>Region Mask</b><br> <img src=qa_mask_ortho.png /> </td>
+    </tr>
+</table>
+
+<h3> Signal Timeseries </h3>
+<table>
+    <tr>
+        <td> <img src=qa_roi_timeseries.png /> </td>
+    <tr>
+</table>
+
+<h3> Motion Timeseries </h3>
+<table>
+    <tr>
+        <td> <img src=qa_motion_timeseries.png /><br> </td>
+    </tr>
+</table>
+
+</body>
 """
+
 
 # Main function
 def main():
@@ -108,12 +173,26 @@ def main():
     
     print('  Creating in vivo QA report for ' + qa_dir)
 
-    # Load QA acquisition info
-    try:
-		    qa_info_file = os.path.join(qa_dir, 'qa_info.txt')
-        # Load metadata from info file
-    except:
-        print('*** No metadata detected in qa_info.txt - skipping')
+    # Determine full path for QA directory
+    qa_dir_abs = os.path.abspath(qa_dir)
+
+    # Load dataset info from QA directory
+    info_fname = os.path.join(qa_dir, 'qa_info.txt')
+    if not os.path.isfile(info_fname):
+        print(info_fname + ' does not exist - exiting')
+        sys.exit(0)
+
+    info = np.genfromtxt(info_fname, delimiter=',')
+    TR_secs = info[0,1]
+    N_vols = info[1,1]
+
+    # Load summary statistics from CSV file in QA directory
+    stats_csv_fname = os.path.join(qa_dir, 'qa_stats.csv')
+    if not os.path.isfile(stats_csv_fname):
+        print(stats_csv_fname + ' does not exist - exiting')
+        sys.exit(0)
+
+    stats = np.genfromtxt(stats_csv_fname, delimiter=',', usecols=(1,2,3,4))
 
     #
     # HTML report generation
@@ -121,7 +200,33 @@ def main():
 
     # Create substitution dictionary for HTML report
     qa_dict = dict([
-      ('phantom_mean',   "%0.1f" % (123.456)),
+        ('qa_dir_abs',     "%s"    % qa_dir_abs),
+        ('TR_secs',        "%0.3f" % TR_secs),
+        ('N_vols',         "%d"    % N_vols),
+        ('brain_tmean',    "%0.1f" % stats[0, 0]),
+        ('brain_thresh',   "%0.1f" % stats[0, 1]),
+        ('brain_nout',     "%d"    % stats[0, 2]),
+        ('brain_pout',     "%0.1f" % stats[0, 3]),
+        ('ghost_tmean',    "%0.1f" % stats[1, 0]),
+        ('ghost_thresh',   "%0.1f" % stats[1, 1]),
+        ('ghost_nout',     "%d"    % stats[1, 2]),
+        ('ghost_pout',     "%0.1f" % stats[1, 3]),
+        ('air_tmean',      "%0.1f" % stats[2, 0]),
+        ('air_thresh',     "%0.1f" % stats[2, 1]),
+        ('air_nout',       "%d"    % stats[2, 2]),
+        ('air_pout',       "%0.1f" % stats[2, 3]),
+        ('dvars_tmean',    "%0.1f" % stats[3, 0]),
+        ('dvars_thresh',   "%0.1f" % stats[3, 1]),
+        ('dvars_nout',     "%d"    % stats[3, 2]),
+        ('dvars_pout',     "%0.1f" % stats[3, 3]),
+        ('dd_um_tmean',    "%0.1f" % stats[4, 0]),
+        ('dd_um_thresh',   "%0.1f" % stats[4, 1]),
+        ('dd_um_nout',     "%d"    % stats[4, 2]),
+        ('dd_um_pout',     "%0.1f" % stats[4, 3]),
+        ('dr_mdeg_tmean',  "%0.1f" % stats[5, 0]),
+        ('dr_mdeg_thresh', "%0.1f" % stats[5, 1]),
+        ('dr_mdeg_nout',   "%d"    % stats[5, 2]),
+        ('dr_mdeg_pout',   "%0.1f" % stats[5, 3]),
     ])
 
     # Generate HTML report from template (see above)
--- cbicqalive_stats.py
+++ cbicqalive_stats.py
@@ -31,204 +31,257 @@
 #
 # Copyright 2013-2014 California Institute of Technology.
 
-import sys
 import os
 import argparse
-import subprocess
-import string
-import numpy as np
 from pylab import *
-from scipy.optimize import curve_fit
+
 
 # Main function
 def main():
 
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(description='CBICQAlive statistics')
+    parser.add_argument('-i', '--qa_dir', help="CBICQAlive directory (*.qalive)")
 
+    # Parse command line arguments
+    args = parser.parse_args()
+    qa_dir = args.qa_dir
+
+    print('  Calculating statistics for ' + qa_dir)
     
-    print('  Start python statistical analysis')
-    print('    Detrending ' + qa_dir)
-
-    # Construct timeseries filename
-    qa_ts_file = os.path.join(qa_dir,'qa_timeseries.txt')
-    if not os.path.isfile(qa_ts_file):
-        print(qa_ts_file + ' does not exist - exiting')
+    # Load ROI timeseries
+    print('    Loading ROI timeseries')
+    qa_ts_fname = os.path.join(qa_dir, 'qa_roi_ts.txt')
+    if not os.path.isfile(qa_ts_fname):
+        print(qa_ts_fname + ' does not exist - exiting')
         sys.exit(0)
 
-    # Load timeseries from QA directory
-    print('    Loading timeseries')
-    x = np.loadtxt(qa_ts_file)
+    x = np.loadtxt(qa_ts_fname)
 
-    # Parse timeseries into column vectors
-    phantom_t = x[:,0]
-    nyquist_t = x[:,1]
-    noise_t   = x[:,2]
+    # Parse timeseries into vectors
+    brain_t = x[:, 0]
+    ghost_t = x[:, 1]
+    air_t = x[:, 2]
 
     # Volume number vector
-    nv = len(phantom_t)
+    nv = len(brain_t)
     v = np.linspace(0, nv-1, nv)
-    
-    #
-    # Fit detrending function
-    #
 
-    # Demean timeseries
-    phantom_mean = phantom_t.mean()
-    nyquist_mean = nyquist_t.mean()
-    noise_mean   = noise_t.mean()
+    # Load DVARS timeseries
+    print('    Loading DVARS timeseries')
+    qa_dvars_fname = os.path.join(qa_dir, 'qa_dvars_ts.txt')
+    if not os.path.isfile(qa_dvars_fname):
+        print(qa_dvars_fname + ' does not exist - exiting')
+        sys.exit(0)
 
-    # Fit exp + linear model to demeaned timeseries
-    print('    Fitting exponential models')
-    phantom_opt, phantom_cov = curve_fit(explin, v, phantom_t - phantom_mean)
-    nyquist_opt, nyquist_cov = curve_fit(explin, v, nyquist_t - nyquist_mean)
-    noise_opt, noise_cov     = curve_fit(explin, v, noise_t - noise_mean)
-    
-    # Generate fitted curves
-    phantom_fit = explin(v, phantom_opt[0], phantom_opt[1], phantom_opt[2]) + phantom_mean
-    nyquist_fit = explin(v, nyquist_opt[0], nyquist_opt[1], nyquist_opt[2]) + nyquist_mean
-    noise_fit   = explin(v, noise_opt[0], noise_opt[1], noise_opt[2]) + noise_mean
-    
-    # Fit residuals
-    phantom_res = phantom_t - phantom_fit
-    nyquist_res = nyquist_t - nyquist_fit
-    noise_res = noise_t - noise_fit
-    
-    # Residual Gaussian sigma estimation
-    # Assumes Gaussian white noise + sparse outlier spikes
-    # Use robust estimate of residual sd (MAD * 1.4826)
-    phantom_res_sigma = np.median(np.abs(phantom_res)) * 1.4826
-    nyquist_res_sigma = np.median(np.abs(nyquist_res)) * 1.4826
-    noise_res_sigma   = np.median(np.abs(noise_res)) * 1.4826
-
-    # Count spikes, defined as residuals more than 5 SD from zero
-    phantom_spikes = (np.abs(phantom_res) > 5 * phantom_res_sigma).sum()
-    nyquist_spikes = (np.abs(nyquist_res) > 5 * nyquist_res_sigma).sum()
-    noise_spikes   = (np.abs(noise_res) > 5 * noise_res_sigma).sum()
-
-    # Calculate percent drift over course of acquisition (use fitted curve)
-    phantom_drift = (phantom_fit[nv-1] - phantom_fit[0]) / phantom_fit[0] * 100.0
-    nyquist_drift = (nyquist_fit[nv-1] - nyquist_fit[0]) / nyquist_fit[0] * 100.0
-    noise_drift = (noise_fit[nv-1] - noise_fit[0]) / noise_fit[0] * 100.0
-
-    # Append mean, spike count, drift to fit parameters
-    phantom_opt = np.append(phantom_opt, [phantom_mean, phantom_spikes, phantom_drift])
-    nyquist_opt = np.append(nyquist_opt, [nyquist_mean, nyquist_spikes, nyquist_drift])
-    noise_opt   = np.append(noise_opt,   [noise_mean, noise_spikes, noise_drift])
-
-    # SNR relative to mean noise
-    # Estimate spatial noise sigma (assuming underlying Gaussian and Half-Normal distribution)
-    # sigma = mean(noise) * sqrt(pi/2)
-    # See for example http://en.wikipedia.org/wiki/Half-normal_distribution
-
-    noise_sigma = noise_mean * sqrt(pi/2)
-    phantom_snr = phantom_mean / noise_sigma
-    nyquist_snr = nyquist_mean / noise_sigma
-
-    # Generate detrended timeseries - add back constant offset for each ROI
-    phantom_0 = phantom_res + phantom_mean
-    nyquist_0 = nyquist_res + nyquist_mean
-    noise_0   = noise_res + noise_mean
-
-    # Create array with detrended timeseries in columns (for output by np.savetxt)
-    ts_detrend = np.array([phantom_0, nyquist_0, noise_0])
-    ts_detrend = ts_detrend.transpose()
-
-    # Create array with timeseries detrend parameters in rows for each model
-    # Parameter order : Exp amp, Exp tau, linear, const, spike count
-    stats_pars = np.row_stack([phantom_opt, nyquist_opt, noise_opt])
+    dvars_t = np.loadtxt(qa_dvars_fname)
 
     #
-    # Center of Mass
-    #
-
-    # Calculate center of mass of temporal mean image using fslstats -c
-    print('    Calculating center of mass of phantom')
-    com_cmd = ["fslstats",os.path.join(qa_dir, "qa_mean"),"-c"]
-    proc = subprocess.Popen(com_cmd, stdout=subprocess.PIPE)
-    out,err = proc.communicate()
-
-    # Split returned string and convert to floats
-    com_x, com_y, com_z = out.split()
-    com_x = float(com_x)
-    com_y = float(com_y)
-    com_z = float(com_z)
-
-    #
-    # Apparent motion parameters
+    # Analyze motion parameters
     #
     print('    Analyzing motion parameters')
     qa_mcf_parfile = os.path.join(qa_dir, 'qa_mcf.par')
 
     if not os.path.isfile(qa_mcf_parfile):
-      print(qa_mcf_parfile + ' does not exist - exiting')
-      sys.exit(0)
+        print(qa_mcf_parfile + ' does not exist - exiting')
+        sys.exit(0)
 
-    # N x 6 array (6 motion parameters in columns)
-    x = np.loadtxt(qa_mcf_parfile)
+    # Read file into 2D array
+    try:
+        mcf_pars = np.loadtxt(qa_mcf_parfile)
+    except:
+        print('*** Could not load motion data from file')
+        sys.exit(1)
 
-    # Extract displacement timeseries for each axis
-    dx = x[:,3]
-    dy = x[:,4]
-    dz = x[:,5]
+    # Extract cumulative rotations and displacements
+    rx, ry, rz = mcf_pars[:, 0], mcf_pars[:, 1], mcf_pars[:, 2]
+    x, y, z = mcf_pars[:, 3], mcf_pars[:, 4], mcf_pars[:, 5]
 
-    # Calculate max absolute displacements (in microns) for each axis
-    max_adx = (np.abs(dx)).max() * 1000.0
-    max_ady = (np.abs(dy)).max() * 1000.0
-    max_adz = (np.abs(dz)).max() * 1000.0
+    # Frame-to-frame displacements and rotations
+    drx, dry, drz = np.gradient(rx), np.gradient(ry), np.gradient(rz)
+    dx, dy, dz = np.gradient(x), np.gradient(y), np.gradient(z)
+
+    # Total F-F rotations and displacements
+    dr, _ = total_rotation(drx, dry, drz)
+    dd = np.sqrt(dx**2 + dy**2 + dz**2)
+
+    # Convert distances from mm to microns
+    dscale = 1000.0
+    x_um, y_um, z_um = x * dscale, y * dscale, z * dscale
+    dd_um, dx_um, dy_um, dz_um = dd * dscale, dx * dscale, dy * dscale, dz * dscale
+
+    # Convert rotations from radians to millidegrees
+    rscale = 180.0 * 1000.0 / np.pi
+    rx_mdeg, ry_mdeg, rz_mdeg = rx * rscale, ry * rscale, rz * rscale
+    dr_mdeg, drx_mdeg, dry_mdeg, drz_mdeg = dr * rscale, drx * rscale, dry * rscale, drz * rscale
 
     #
-    # Finalize stats array for writing
+    # Summary statistics for all timeseries
     #
-    
-    # Flatten stats array (phantom pars first, then nyquist, etc)
-    stats_pars = stats_pars.flatten()
-
-    # Add remaining stats to bottom of array
-    stats_pars = np.append(stats_pars, [phantom_snr, nyquist_snr, com_x, com_y, com_z, max_adx, max_ady, max_adz])
+    brain_tmean, brain_thresh, brain_nout, brain_pout, _ = timeseries_stats(brain_t)
+    ghost_tmean, ghost_thresh, ghost_nout, ghost_pout, _ = timeseries_stats(ghost_t)
+    air_tmean, air_thresh, air_nout, air_pout, _ = timeseries_stats(air_t)
+    dvars_tmean, dvars_thresh, dvars_nout, dvars_pout, _ = timeseries_stats(dvars_t)
+    dd_um_tmean, dd_um_thresh, dd_um_nout, dd_um_pout, _ = timeseries_stats(dd_um)
+    dr_mdeg_tmean, dr_mdeg_thresh, dr_mdeg_nout, dr_mdeg_pout, _ = timeseries_stats(dr_mdeg)
 
     #
-    # Output results to files
+    # Output summary statistics to CSV file
     #
+    stats_csv_fname = os.path.join(qa_dir, 'qa_stats.csv')
+    fd = open(stats_csv_fname, "w")
 
-    # Save fit parameters
-    qa_stats_parfile = os.path.join(qa_dir,'qa_stats.txt')
-    np.savetxt(qa_stats_parfile, stats_pars, delimiter=' ',fmt='%0.6f')
+    fd.write("Brain, %0.1f, %0.1f, %d, %0.1f\n" % (brain_tmean, brain_thresh, brain_nout, brain_pout))
+    fd.write("Ghost, %0.1f, %0.1f, %d, %0.1f\n" % (ghost_tmean, ghost_thresh, ghost_nout, ghost_pout))
+    fd.write("Air, %0.1f, %0.1f, %d, %0.1f\n" % (air_tmean, air_thresh, air_nout, air_pout))
+    fd.write("DVARS, %0.1f, %0.1f, %d, %0.1f\n" % (dvars_tmean, dvars_thresh, dvars_nout, dvars_pout))
+    fd.write("FF_Disp_um, %0.1f, %0.1f, %d, %0.1f\n" % (dd_um_tmean, dd_um_thresh, dd_um_nout, dd_um_pout))
+    fd.write("FF_Rot_mdeg, %0.1f, %0.1f, %d, %0.1f\n" % (dr_mdeg_tmean, dr_mdeg_thresh, dr_mdeg_nout, dr_mdeg_pout))
 
-    # Save detrended timeseries
-    print('    Writing detrended timeseries')
-    ts_detrend_file = os.path.join(qa_dir,'qa_timeseries_detrend.txt')
-    np.savetxt(ts_detrend_file, ts_detrend, delimiter=' ',fmt='%0.6f')
+    fd.close()
 
-    # Plot figures
-    fig = figure(figsize = (10,8))
+    # ROI timeseries figure
+
+    fig = figure(figsize=(10, 10))
 
     subplot(411)
-    plot(v, phantom_t, 'ro', v, phantom_fit, '-b', v, phantom_0, '-g')
-    title("Phantom", x = 0.5, y = 0.8)
+    plot(v, brain_t)
+    title("Mean Brain Signal", x=0.5, y=0.8)
     
     subplot(412)
-    plot(v, nyquist_t, 'ro', v, nyquist_fit, '-b', v, nyquist_0, '-g')
-    title("Nyquist", x = 0.5, y = 0.8)
+    plot(v, ghost_t)
+    title("Mean Ghost Signal", x=0.5, y=0.8)
     
     subplot(413)
-    plot(v, noise_t, 'ro', v, noise_fit, '-b', v, noise_0, '-g')
-    title("Noise", x = 0.5, y = 0.8)
-    
+    plot(v, air_t)
+    title("Mean Air Signal", x=0.5, y=0.8)
+
     subplot(414)
-    plot(v, dx, 'r', v, dy, 'g', v, dz, 'b')
-    title("Displacement (mm)", x = 0.5, y = 0.8)
+    plot(v, dvars_t)
+    title("DVARS", x=0.5, y=0.8)
 
-    # Pack all subplots and labels tightly
-    fig.subplots_adjust(hspace = 0.2)
+    savefig(os.path.join(qa_dir, 'qa_roi_timeseries.png'), dpi=72, bbox_inches='tight')
 
-    # Save figure in QA directory
-    savefig(os.path.join(qa_dir,'qa_timeseries.png'), dpi = 72, bbox_inches = 'tight')
+    # Motion timeseries figure
+
+    fig.clf()
+
+    subplot(411)
+    plot(v, x_um, 'r', v, y_um, 'g', v, z_um, 'b')
+    title("Displacement (microns)", x=0.5, y=0.8)
+
+    subplot(412)
+    plot(v, rx_mdeg, 'r', v, ry_mdeg, 'g', v, rz_mdeg, 'b')
+    title("Rotation (mdeg)", x=0.5, y=0.8)
+
+    subplot(413)
+    plot(v, dd_um)
+    title("Total F-F Displacement (microns)", x=0.5, y=0.8)
+
+    subplot(414)
+    plot(v, dr_mdeg)
+    title("Total F-F Rotation (mdeg)", x=0.5, y=0.8)
+
+    savefig(os.path.join(qa_dir, 'qa_motion_timeseries.png'), dpi=72, bbox_inches='tight')
 
     # Done
     print('  Finished python statistical analysis')
 
-# Exponential + linear detrending model
-def explin(t, a, tau, b):
-    return a * np.exp(-t/tau) + b * t
+
+def total_rotation(rx, ry, rz):
+    """
+    Total rotation and axes for a three-axis gimble rotation
+    Adapted from the discussion thread at http://www.mathworks.com/matlabcentral/newsreader/view_thread/160945
+
+    :param rx: numpy array
+    :param ry: numpy array
+    :param rz: numpy array
+    :return: rtot, raxes
+
+    """
+    # Find length of angle vectors
+    n = rx.shape[0]
+
+    # Make space for results
+    rtot = np.zeros([n, 1])
+    raxes = np.zeros([n, 3])
+
+    for i, tx in enumerate(rx):
+
+        # Construct total rotation matrix
+        ctx = np.cos(tx)
+        stx = np.sin(tx)
+        rotx = np.array([[1, 0, 0], [0, ctx, -stx], [0, stx, ctx]])
+
+        cty = np.cos(ry[i])
+        sty = np.sin(ry[i])
+        roty = np.array([[cty, 0, sty], [0, 1, 0], [-sty, 0, cty]])
+
+        ctz = np.cos(rz[i])
+        stz = np.sin(rz[i])
+        rotz = np.array([[ctz, -stz, 0], [stz, ctz, 0], [0, 0, 1]])
+
+        # Total rotation matrix is product of axis rotations
+        rotall = np.dot(rotz, np.dot(roty, rotx))
+
+        # Direct calculation of angle and axis from A
+        # Code adapted from thread response by Bruno Luong
+
+        # Rotation axis u = [x, y, z]
+        u = np.array([rotall[2, 1]-rotall[1, 2], rotall[0, 2]-rotall[2, 0], rotall[1, 0]-rotall[0, 1]])
+
+        # Rotation sine and cosine
+        c = np.trace(rotall) - 1
+        s = np.linalg.norm(u)
+
+        # Total rotation in radians
+        rtot[i] = np.arctan2(s, c)
+
+        # Adjust rotation to be positive, flipping axis if necessary
+        if s > 0:
+            u /= s
+        else:
+            # warning('A close to identity, arbitrary result');
+            u = [1, 0, 0]
+
+        # Save axis result
+        raxes[i, :] = u
+
+    return rtot, raxes
+
+
+def timeseries_stats(s):
+    """
+    Compute useful stats for numpy timeseries vector
+    Identify upper outliers in timeseries using UQ + 1.5 * IQR
+    :param s: numpy vector of real values
+    :return: tmean, thresh, nout, iout
+        tmean = temporal mean of vector
+        thresh = outlier threshold
+        nout = number of outlier values
+        iout = indices of outliers in timeseries
+    """
+
+    # Temporal mean of vector
+    tmean = s.mean()
+
+    # Find LQ and UQ of values
+    lq, uq = np.percentile(s, (25, 75))
+
+    # Outlier threshold = UQ + 1.5 (UQ - LQ) = 2.5 UQ - LQ
+    thresh = 2.5 * uq - lq
+
+    # Find indices of outliers in vector
+    iout = np.array(np.where(s > thresh))
+
+    # Count the outliers
+    nout = iout.size
+
+    # Percent of values that are outliers
+    pout = nout / float(s.size) * 100.0
+
+    return tmean, thresh, nout, pout, iout
+
 
 # This is the standard boilerplate that calls the main() function.
 if __name__ == '__main__':
